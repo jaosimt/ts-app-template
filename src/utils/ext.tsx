ff793d12ef6dc39198035f139b24922cbccbe50d
Sim Jao <jao.sim@gmail.com>
2026-02-23 18:18:24 +0800

    cleanup

--- src/utils/ext.tsx
+++ src/utils/ext.tsx
@@ -1,4 +1,4 @@
 import { ReactNode } from 'react';
 
 export const createLink = (label: string | ReactNode, url: string, target?: '_blank' | '_self' | '_parent' | '_top') =>
-    <a target={target || '_blank'} href={url}><b>{label}</b></a>;
+    <a target={target || '_blank'} href={url} rel='noreferrer'><b>{label}</b></a>;
