ec1a64096a03d74b74242ac8b8e6bf7b1236c067
Sim Jao <jao.sim@gmail.com>
2026-03-02 18:44:26 +0800

    added prop disabled

--- src/components/partials/checkbox/index.tsx
+++ src/components/partials/checkbox/index.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { CSSUnit } from '../../../types';
 import { parseCSSUnit } from '../../../utils';
 
-const CheckboxContainer = styled.div`
+const CheckboxContainer = styled.div<{disabled: boolean}>`
     display: inline-flex;
     align-items: center;
     height: 33px;
@@ -28,19 +28,22 @@ const HiddenCheckbox = styled.input.attrs({type: 'checkbox'})`
 `;
 
 const StyledCheckbox = styled.div<{
-    checked: boolean | undefined
+    checked: boolean | undefined,
+    disabled: boolean
 }>`
     display: inline-block;
     width: 21px;
     height: 21px;
     border-radius: 4px;
-    border: 1px solid rgb(0, 123, 255);
+    border: 1px solid ${props => props.disabled ? 'gainsboro' : 'rgb(0, 123, 255)'};
     background-color: ${props => props.checked ? 'rgba(0, 123, 255, 0.63)' : 'transparent'};
     transition: all 150ms;
     
     ${Icon} {
         visibility: ${props => (props.checked ? 'visible' : 'hidden')}
     }
+    
+    ${props => props.disabled && 'opacity: 0.7; pointer-events: none;'}
 `;
 
 const Label = styled.label`
@@ -48,6 +51,7 @@ const Label = styled.label`
     user-select: none;
     display: inline-flex;
     align-items: center;
+    width: fit-content;
     
     &:hover {
         > ${CheckboxContainer} {
@@ -56,20 +60,21 @@ const Label = styled.label`
     }
 `;
 
-const Checkbox = ({label, labelWidth, name, className, checked, labelPosition, onChange, ...props}: {
+const Checkbox = ({disabled = false, label, labelWidth, name, className, checked, labelPosition, onChange, ...props}: {
     labelWidth?: CSSUnit,
     label?: string,
     labelPosition?: 'left' | 'right',
     name: string,
     className?: string,
     checked: boolean | undefined,
-    onChange?: any
+    onChange?: any,
+    disabled?: boolean
 }) => (
     <Label>
         {labelPosition === 'left' && <span style={{width: parseCSSUnit(labelWidth as CSSUnit), marginRight: '0.3rem'}}>{label || name}</span>}
-        <CheckboxContainer className={className}>
-            <HiddenCheckbox name={name} checked={checked} onChange={onChange} {...props} />
-            <StyledCheckbox checked={checked}>
+        <CheckboxContainer disabled={disabled} className={className}>
+            <HiddenCheckbox disabled={disabled} name={name} checked={checked} onChange={onChange} {...props} />
+            <StyledCheckbox disabled={disabled} checked={checked}>
                 <Icon viewBox="0 0 24 24">
                     <polyline points="20 6 9 17 4 12"/>
                 </Icon>
