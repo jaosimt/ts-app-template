ce78b046d759e6635788e408d6066635916da6e0
Sim Jao <jao.sim@gmail.com>
2026-03-15 17:32:28 +0800

    * add min-height to demo pages wrapper * fixed and add animation to CollapsibleLink component * added text-decoration underline to link that is not nav_link

--- src/components/pages/demo/index.tsx
+++ src/components/pages/demo/index.tsx
@@ -1,23 +1,23 @@
-import {FC} from 'react';
-import {NavLink, Outlet, useLocation} from 'react-router';
+import { FC } from 'react';
+import { NavLink, Outlet, useLocation } from 'react-router';
 import styled from 'styled-components';
 import { Theme } from '../../../constants';
 import { ThemeProp } from '../../../constants/interfaces';
-import {classNames} from '../../../utils';
+import { classNames } from '../../../utils';
 import UnderConstruction from '../underConstruction';
 import {
     $primaryColor, $primaryColorDark, $textColor, $textColorDark
 } from '../../../styles/variables';
 
 const Container = styled.div`
-    display    : flex;
-    gap        : 1rem;
-    width      : 100%;
-    min-height : 490px;
+    display: flex;
+    gap: 1rem;
+    width: 100%;
+    min-height: 490px;
 
-    @media (max-width : 768px) {
-        flex-direction : column;
-        gap            : 0.5rem;
+    @media (max-width: 768px) {
+        flex-direction: column;
+        gap: 0.5rem;
     }
 `;
 
@@ -25,33 +25,33 @@ const Container = styled.div`
 const Nav = styled.nav<{
     $theme: ThemeProp;
 }>`
-    display        : flex;
-    flex-direction : column;
+    display: flex;
+    flex-direction: column;
 
     a {
-        border  : 1px solid transparent;
-        padding : 0.5rem;
-        color   : ${props => props.$theme === Theme.DARK ? $textColorDark : $textColor} !important;
+        border: 1px solid transparent;
+        padding: 0.5rem;
+        color: ${props => props.$theme === Theme.DARK ? $textColorDark : $textColor} !important;
 
         &:hover {
-            color : ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor} !important;
+            color: ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor} !important;
         }
 
         &.active {
-            border-bottom-color : ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor};
-            border-top-color    : ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor};
-            color               : ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor} !important;
+            border-bottom-color: ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor};
+            border-top-color: ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor};
+            color: ${props => props.$theme === Theme.DARK ? $primaryColorDark : $primaryColor} !important;
         }
     }
 
-    @media (max-width : 768px) {
-        display               : grid;
-        grid-template-columns : repeat(4, 1fr);
-        font-size             : small;
+    @media (max-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        font-size: small;
 
         a {
-            padding    : 0.1rem;
-            text-align : center;
+            padding: 0.1rem;
+            text-align: center;
         }
     }
 `;
@@ -59,27 +59,29 @@ const Nav = styled.nav<{
 const Wrapper = styled.div<{
     $theme: ThemeProp;
 }>`
-    padding          : 1.5rem;
-    border-radius    : 0.3rem;
-    position         : relative;
-    width            : calc(100% - 150px);
-    background-color : ${props => props.$theme === Theme.DARK ? '#221614' : '#f7e9e2'};
-    opacity          : 0.9;
+    padding: 1.5rem;
+    border-radius: 0.3rem;
+    position: relative;
+    width: calc(100% - 150px);
+    background-color: ${props => props.$theme === Theme.DARK ? '#221614' : '#f7e9e2'};
+    opacity: 0.9;
+    min-height: calc(100vh - 9rem);
 
-    @media (max-width : 768px) {
-        width            : 100%;
-        height           : 100%;
-        padding          : 0;
-        background-color : transparent;
+    @media (max-width: 768px) {
+        min-height: unset;
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        background-color: transparent;
     }
 `;
 
 const InnerWrapper = styled.div`
-    display        : flex;
-    flex-direction : column;
-    gap            : 1rem;
-    width          : 100%;
-    height         : 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 100%;
+    height: 100%;
 `;
 
 const Demo: FC<{ theme: ThemeProp }> = ({theme}) => {
--- src/components/partials/collapsibleLink/index.tsx
+++ src/components/partials/collapsibleLink/index.tsx
@@ -1,18 +1,59 @@
-import { FC, useState } from 'react';
-import { classNames } from '../../../utils';
+import { FC, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { CSSUnit } from '../../../constants/types';
+import { parseCSSUnit } from '../../../utils';
 
 export interface CollapsibleLinkProps {
     linkText: string;
     details: any;
     detailsClassName?: string;
+    position?: 'fixed' | 'relative' | 'absolute';
 }
 
-const CollapsibleLink:FC<CollapsibleLinkProps> = ({linkText, details, detailsClassName}) => {
-    const [show, setShow] = useState(false);
+const StyledPre = styled.pre<{
+    $height: CSSUnit;
+    $width: CSSUnit;
+    $position: 'relative' | 'fixed' | 'absolute';
+}>`
+    display: block;
+    height: ${props => `${parseCSSUnit(props.$height)} !important;`};
+    margin: 0;
+    color: gray;
+    font-size: x-small;
+    width: ${props => parseCSSUnit(props.$width)};
+    transition: all 200ms ease-in-out;
+    overflow-y: hidden;
+    position: ${props => props.$position};
+    ${props => {
+        if ([0, 'auto'].includes(props.$height))
+            return `overflow-x: hidden; visibility: hidden; opacity: 0; z-index: -1; background-color: transparent !important; padding: 0 !important;`
+        else
+            return `overflow-x: auto; visibility: visible; opacity: 1; margin-top: 0.1rem;`
+    }};
+`;
 
-    return <span className={'display-flex flex-direction-column trim'}>
-        <b className={'link'} onClick={() => setShow(!show)}>{linkText}</b>
-        {show && <pre className={classNames(detailsClassName, 'm-0 mt-0p1', 'overflow-auto', 'color-gray', 'font-size-x-small')}>{details}</pre>}
+const CollapsibleLink:FC<CollapsibleLinkProps> = ({linkText, details, detailsClassName, position: props_position = 'relative'}) => {
+    const bRef = useRef<HTMLElement>(null);
+    const preRef = useRef<HTMLPreElement>(null);
+    const heightRef = useRef<CSSUnit>('auto');
+    const widthRef = useRef<CSSUnit>('auto');
+    const [height, setHeight] = useState<CSSUnit>('auto');
+    const [position, setPosition] = useState<'fixed'|'relative' | 'absolute'>('fixed');
+
+    useEffect(() => {
+        if (preRef.current) {
+            heightRef.current = preRef.current.getBoundingClientRect().height + 14;
+            setHeight(0);
+            setPosition(props_position)
+        }
+        if (bRef.current) widthRef.current = bRef.current.getBoundingClientRect().width;
+
+        // eslint-disable-next-line
+    }, []);
+
+    return <span className={'trim'}>
+        <b style={{display: 'block'}} ref={bRef} className={'link disable-select'} onClick={() => setHeight(height === 0 ? heightRef.current : 0 )}>{linkText}</b>
+        <StyledPre $position={position} $width={widthRef.current} $height={height} ref={preRef} className={detailsClassName}>{details}</StyledPre>
     </span>
 }
 
