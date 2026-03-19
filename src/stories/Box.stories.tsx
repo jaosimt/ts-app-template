3b9118b3a3632fefe0ed630d926d2a26363ae686
Sim Jao <jao.sim@gmail.com>
2026-03-20 04:46:11 +0800

    partial storybook

---
+++ src/stories/Box.stories.tsx
@@ -0,0 +1,56 @@
+// @ts-ignore
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import Box, { BoxProps } from '../components/partials/box';
+import '../index.scss';
+import '../styles/tags.scss';
+import '../styles/common.scss';
+import '../styles/animations.scss';
+import '../styles/theme.scss';
+import '../styles/tippy.scss';
+
+const meta = {
+    title: 'Custom Components/Box',
+    component: Box,
+    parameters: {
+        layout: 'centered',
+        controls: { exclude: ['children', 'options'] },
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        labelPosition: {
+            control: 'select',
+            options: ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+        },
+        labelSize: {
+            control: 'select',
+            options: ['small', 'medium', 'large']
+        },
+    }
+} satisfies Meta<BoxProps>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        children: <h1>Box</h1>,
+    },
+};
+
+export const Propped: Story = {
+    args: {
+        children: <h1>Box</h1>,
+        label: 'My Box',
+        backgroundColor: 'red',
+        borderColor: 'red',
+        labelColor: 'red',
+        labelBackgroundColor: 'white',
+        borderRadius: 7,
+        padding: 14,
+        width: 300,
+        border: true,
+        tight: false,
+        labelPosition: 'top-left',
+        labelSize: 'small',
+    },
+};
---
+++ src/stories/Dropdown.stories.tsx
@@ -0,0 +1,95 @@
+// @ts-ignore
+import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { FaDog } from 'react-icons/fa6';
+import { GiFox, GiRiver } from 'react-icons/gi';
+import { IoIosArrowDropdown, IoIosArrowDropdownCircle, IoMdArrowDropdown } from 'react-icons/io';
+import { LiaPoopSolid } from 'react-icons/lia';
+import { RiDropdownList } from 'react-icons/ri';
+import { RxDropdownMenu } from 'react-icons/rx';
+import Dropdown, { DropdownProps } from '../components/partials/dropdown';
+import '../index.scss';
+import '../styles/tags.scss';
+import '../styles/common.scss';
+import '../styles/animations.scss';
+import '../styles/theme.scss';
+import '../styles/tippy.scss';
+
+const icons = {IoIosArrowDropdown, IoIosArrowDropdownCircle, IoMdArrowDropdown, RiDropdownList, RxDropdownMenu};
+
+const meta = {
+    title: 'Custom Components/Dropdown',
+    component: Dropdown,
+    parameters: {
+        // layout: 'centered',
+        controls: {exclude: ['children', 'options', 'selected']},
+    },
+    // tags: ['autodocs'],
+} satisfies Meta<DropdownProps>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const ddOptions = [
+    'The quick brown fox',
+    'jumps over the lazy dog',
+    'near the bunk of the river',
+    'and I wonder who cares!'
+];
+
+const ddOOptions = [
+    {
+        label: <span className={'color-cyan'}>The quick brown fox</span>,
+        value: 'The quick brown fox',
+        icon: GiFox
+    }, {
+        label: <span className={'color-magenta'}>jumps over the lazy dog</span>,
+        value: 'jumps over the lazy dog',
+        icon: FaDog
+    }, {
+        label: <span className={'color-yellow'}>near the bunk of the river</span>,
+        value: 'near the bunk of the river',
+        icon: GiRiver
+    }, {
+        label: <span className={'color-black'}>and I wonder who cares!</span>,
+        value: 'and I wonder who cares!',
+        icon: LiaPoopSolid
+    }
+];
+
+export const StringOptions: Story = {
+    args: {
+        selected: ddOptions[0],
+        options: ddOptions
+    },
+};
+
+export const ObjectOptions: Story = {
+    args: {
+        selected: ddOOptions[0],
+        options: ddOOptions
+    },
+};
+
+export const Propped: Story = {
+    argTypes: {
+        labelAlign: {
+            control: 'select',
+            options: ['left', 'right', 'center', 'space-between']
+        },
+        icon: {
+            options: Object.keys(icons),
+            mapping: icons,
+            control: { type: 'select' },
+        }
+    },
+    args: {
+        label: 'My Object Dropdown',
+        selected: ddOOptions[0],
+        options: ddOOptions,
+        icon: RxDropdownMenu,
+        labelWidth: 200,
+        labelAlign: 'space-between',
+        disabled: false,
+    },
+};
