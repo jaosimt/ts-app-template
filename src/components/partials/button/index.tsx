b923b0b96a5ac760d552656ce1fd5c0a57810184
Sim Jao <jao.sim@gmail.com>
2026-03-22 15:53:38 +0800

    refactor Button component to enhance theme handling, replace SCSS with styled-components, and clean up redundant styles

--- src/components/partials/button/index.tsx
+++ src/components/partials/button/index.tsx
@@ -6,7 +6,13 @@ import { CSSUnit } from '../../../constants/types';
 import { useAppSelector } from '../../../hooks';
 import { getTheme } from '../../../slices/theme';
 import { getRandStr, parseCSSUnit } from '../../../utils';
+import {
+    getButtonDefaultBorderColor, getButtonDefaultHoverColor, getButtonDefaultTextColor,
+    getButtonPrimaryBorderColor,
+    getButtonPrimaryColor, getButtonPrimaryHoverColor
+} from '../../../utils/themeUtils';
 import { ReactIcon } from '../index';
+import v from '../../../styles/variables.module.scss';
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
     type?: 'button' | 'submit' | 'reset';
@@ -22,60 +28,100 @@ export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
 const Container = styled.button<{
     $theme: ThemeProp
 }>`
-    box-shadow   : 0 0 7px #fff;
-    margin       : 0 0.3rem;
+    box-shadow: 0 0 7px #fff;
+    margin: 0 0.3rem;
 
-    display      : inline-flex;
-    min-width    : fit-content;
-    width        : fit-content;
-    border-width : 1px;
-    border-style : solid;
-    cursor       : pointer;
-    transition   : all 150ms ease-in-out;
-    align-items  : center;
-    gap          : 0.5rem;
+    display: inline-flex;
+    min-width: fit-content;
+    width: fit-content;
+    border-width: 1px;
+    border-style: solid;
+    cursor: pointer;
+    transition: all 150ms ease-in-out;
+    align-items: center;
+    gap: 0.5rem;
 
     &.btn-icon {
-        padding       : 2px;
-        border-radius : 50%;
+        padding: 2px;
+        border-radius: 50%;
+        background-color: ${v.buttonPrimaryTextColor};
+        border-color: ${props => getButtonPrimaryBorderColor(props.$theme)};
+        color: ${props => getButtonPrimaryColor(props.$theme)};
     }
 
     &:not(.btn-icon) {
-        @extend .border-radius-0p3;
-        display         : inline-flex;
-        align-items     : center;
-        justify-content : center;
-        padding         : 0.35rem 0.42rem;
-        min-width       : 33px;
-        min-height      : 33px;
+        border-radius: 0.3rem;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0.35rem 0.42rem;
+        min-width: ${v.inputHeight};
+        min-height: ${v.inputHeight};
+
+        &.default {
+            background-color: ${v.buttonDefaultColor};
+            border-color: ${props => getButtonDefaultBorderColor(props.$theme)};
+            color: ${props => getButtonDefaultTextColor(props.$theme)};
+        }
+
+        &:not(.default) {
+            background-color: ${props => getButtonPrimaryColor(props.$theme)};
+            border-color: ${props => getButtonPrimaryBorderColor(props.$theme)};
+            color: ${v.buttonPrimaryTextColor};
+        }
 
         &.no-padding {
-            padding : 0;
+            padding: 0;
         }
     }
 
     &:hover {
-        &:active { opacity : 0.3; }
+        &.btn-icon {
+            color: ${props => getButtonPrimaryHoverColor(props.$theme)};
+            background-color: v.$buttonPrimaryTextColor;
+        }
+
+        &:not(.btn-icon):not(.no-hover) {
+            &.default {
+                background-color: ${props => getButtonDefaultHoverColor(props.$theme)};
+                color: v.$buttonDefaultColor;
+            }
+
+            &:not(.default) {
+                background-color: ${props => getButtonPrimaryHoverColor(props.$theme)};
+            }
+        }
+
+        &:active { opacity: 0.3; }
     }
 
     &[disabled],
     &.disabled {
-        pointer-events : none !important;
-        opacity        : 0.3;
+        pointer-events: none !important;
+        opacity: 0.3;
+
+        &.default {
+            border-color: ${v.buttonDefaultDisabledTextColor};
+            color: ${v.buttonDefaultDisabledTextColor};
+        }
+
+        &:not(.default) {
+            border-color: ${v.buttonPrimaryDisabledTextColor};
+        }
     }
-    
+
     @media (max-width: 768px) {
-        font-size : small;
-        gap       : 0.3rem;
+        font-size: small;
+        gap: 0.3rem;
 
         &.btn-icon {
-            padding : 1px;
+            padding: 1px;
         }
 
         &:not(.btn-icon) {
-            padding    : 0.1rem 0.4rem;
-            min-width  : 28px;
-            min-height : 28px;
+            padding: 0.1rem 0.4rem;
+            min-width: ${v.inputHeightSmall};
+            min-height: ${v.inputHeightSmall};
         }
     }
 `;
@@ -114,10 +160,10 @@ const Button: FC<ButtonProps> = (props) => {
                 className={iconClassName}
                 style={{position: !isBtnIcon && !children ? 'absolute' : 'relative'}}
                 size={!isBtnIcon && !children ? 21 : 14}
-                icon={icon}/>
+                icon={icon} />
         }
         {children}
     </Container>;
-}
+};
 
 export default memo(Button);
